db16744d5d9881c9913603558ff5a536b8fa60d7
OussamaDev <oussama.lahmidi@icloud.com>
2020-05-03 07:26:59 +0000

    Added Service & Direction

---
+++ src/app/models/direction.ts
@@ -0,0 +1,6 @@
+
+export interface Direction {
+      id?: number;
+      nom: string;
+      // statut: string
+    }
---
+++ src/app/models/service.ts
@@ -0,0 +1,6 @@
+
+export interface Service {
+      id?: number;
+      nom: string;
+      // statut: string
+    }
