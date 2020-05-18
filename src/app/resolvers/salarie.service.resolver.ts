7737649c88a3aa923388ca3c315d86f4d5d55b78
OussamaDev <oussama.lahmidi@icloud.com>
2020-05-18 14:58:33 +0000

    - Added salarie not found (error 404) handler. - Tweaked code.

--- src/app/resolvers/salarie.service.resolver.ts
+++ src/app/resolvers/salarie.service.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { catchError } from 'rxjs/operators'; import { empty, Observable } from 'rxjs';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { catchError, map } from 'rxjs/operators'; import { empty, Observable, of } from 'rxjs';
 import { PosteService } from '../services/poste.service';
 import { Poste } from '../models/poste';
 import { User } from '../models/user';
@@ -15,11 +15,26 @@ import {SalariesService} from '../services/salaries.service';
 
 export class SalarieServiceResolver implements Resolve<Salarie> {
 
-  constructor(private service: SalariesService) { }
+  constructor(private service: SalariesService, private router: Router) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Salarie> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
     console.log(this.service.getSalarie(parseInt(route.paramMap.get('id'))));
-    return this.service.getSalarie(parseInt(route.paramMap.get('id')));
+    return this.service.getSalarie(parseInt(route.paramMap.get('id')))
+    .pipe(
+      map (salarie => salarie),
+      catchError(error => {
+        console.log(error);
+        if (error.status == 404) {
+          alert("Ce salarié est introuvable");
+          this.router.navigate(['/home/salaries']);
+        }
+        else {
+          alert("Something went wrong...");
+          this.router.navigate(['/home/dashboard']);
+        }
+        return of({ salarie: null });
+      })
+    );
   }
 
 }
