958670293ad5101533008aa77def5b1b1ad02182
OussamaDev <oussama.lahmidi@icloud.com>
2020-05-26 12:59:04 +0000

    - Updated express server to match the new configuration.

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/browser'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
